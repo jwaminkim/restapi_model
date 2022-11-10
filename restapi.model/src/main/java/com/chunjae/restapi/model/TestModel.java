29f37cced15922ca5eba1dc375db02f2547a1dd4
jmkim <sir_jm@naver.com>
2022-11-10 10:25:41 +0900

    20221110 1025

--- restapi.model/src/main/java/com/chunjae/restapi/model/TestModel.java
+++ restapi.model/src/main/java/com/chunjae/restapi/model/TestModel.java
@@ -5,5 +5,13 @@ public class TestModel {
 	public String Name;		
 	public String Id;
 	public String Nick;
+		
+	public TestModel(){
+		
+		this.Name = "김좌민";
+		this.Id = "jwaminkim";
+		this.Nick = "123";		
+				
+	}
 
 }
